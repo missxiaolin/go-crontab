ab2069f2964b8abd516e3d7d426e3f5dde1393da
xiaolin <462441355@qq.com>
2019-06-16 11:16:29 +0800

    * 过期

--- prepare/etcd_usage/main.go
+++ prepare/etcd_usage/main.go
@@ -12,6 +12,7 @@ var (
 	client *clientv3.Client
 	err error
 	kv clientv3.KV
+	lease clientv3.Lease
 )
 
 func init()  {
@@ -29,12 +30,82 @@ func init()  {
 
 	// 用户读写etcd的键值对
 	kv = clientv3.NewKV(client)
+
+	// 申请一个lease（租约）
+	lease = clientv3.NewLease(client)
 }
 
 func main() {
 	//etcdPut()
 	//etcdGet()
 	//etcdDel()
+	etcdLease()
+}
+
+func etcdLease()  {
+	var (
+		leaseGrantResp *clientv3.LeaseGrantResponse
+		leaseId clientv3.LeaseID
+		putResp *clientv3.PutResponse
+		getResp *clientv3.GetResponse
+		keepResp *clientv3.LeaseKeepAliveResponse
+		keepRespChan <-chan *clientv3.LeaseKeepAliveResponse
+	)
+
+	// 申请一个10秒的租约
+	if leaseGrantResp, err = lease.Grant(context.TODO(), 10); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// 拿到租约的ID
+	leaseId = leaseGrantResp.ID
+
+	// 5秒后会取消自动续租
+	//ctx, _ := context.WithTimeout(context.TODO(), 5 * time.Second)
+
+	if keepRespChan, err = lease.KeepAlive(context.TODO(), leaseId); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// 处理续约应答的协程
+	go func() {
+		for {
+			select {
+			case keepResp = <- keepRespChan:
+				if keepRespChan == nil {
+					fmt.Println("租约已经失效了")
+					goto END
+				} else {	// 每秒会续租一次, 所以就会受到一次应答
+					fmt.Println("收到自动续租应答:", keepResp.ID)
+				}
+			}
+		}
+	END:
+	}()
+
+	// Put一个KV, 让它与租约关联起来, 从而实现10秒后自动过期
+	if putResp, err = kv.Put(context.TODO(), "name", "xiaolin", clientv3.WithLease(leaseId)); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("写入成功:", putResp.Header.Revision)
+
+	// 定时的看一下key过期了没有
+	for {
+		if getResp, err = kv.Get(context.TODO(), "name"); err != nil {
+			fmt.Println(err)
+			return
+		}
+		if getResp.Count == 0 {
+			fmt.Println("kv过期了")
+			break
+		}
+		fmt.Println("还没过期:", getResp.Kvs)
+		time.Sleep(2 * time.Second)
+	}
 }
 
 func etcdDel() {
