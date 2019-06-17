ee72f32c45a3424b824d54ccd536932f483bd7d7
xiaolin <462441355@qq.com>
2019-06-17 10:22:51 +0800

    * 锁测试

--- prepare/etcd_usage/main.go
+++ prepare/etcd_usage/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"github.com/coreos/etcd/clientv3"
+	"github.com/coreos/etcd/mvcc/mvccpb"
 	"time"
 )
 
@@ -39,7 +40,160 @@ func main() {
 	//etcdPut()
 	//etcdGet()
 	//etcdDel()
-	etcdLease()
+	//etcdLease()
+	//etcdWatch()
+	etcdTxn()
+}
+
+func etcdTxn()  {
+	var (
+		leaseGrantResp *clientv3.LeaseGrantResponse
+		leaseId clientv3.LeaseID
+		keepRespChan <-chan *clientv3.LeaseKeepAliveResponse
+		keepResp *clientv3.LeaseKeepAliveResponse
+		ctx context.Context
+		cancelFunc context.CancelFunc
+		kv clientv3.KV
+		txn clientv3.Txn
+		txnResp *clientv3.TxnResponse
+	)
+
+	// lease实现锁自动过期:
+	// op操作
+	// txn事务: if else then
+
+	// 申请一个5秒的租约
+	if leaseGrantResp, err = lease.Grant(context.TODO(), 5); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// 拿到租约的ID
+	leaseId = leaseGrantResp.ID
+
+	// 准备一个用于取消自动续租的context
+	ctx, cancelFunc = context.WithCancel(context.TODO())
+
+	// 确保函数退出后, 自动续租会停止
+	defer cancelFunc()
+	defer lease.Revoke(context.TODO(), leaseId)
+
+	// 5秒后会取消自动续租
+	if keepRespChan, err = lease.KeepAlive(ctx, leaseId); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// 处理续约应答的协程
+	go func() {
+		for {
+			select {
+			case keepResp = <- keepRespChan:
+				if keepRespChan == nil {
+					fmt.Println("租约已经失效了")
+					goto END
+				} else {	// 每秒会续租一次, 所以就会受到一次应答
+					fmt.Println("收到自动续租应答:", keepResp.ID)
+				}
+			}
+		}
+	END:
+	}()
+
+	//  if 不存在key， then 设置它, else 抢锁失败
+	kv = clientv3.NewKV(client)
+
+	// 创建事务
+	txn = kv.Txn(context.TODO())
+
+	// 定义事务
+
+	// 如果key不存在
+	txn.If(clientv3.Compare(clientv3.CreateRevision("/cron/lock/job9"), "=", 0)).
+		Then(clientv3.OpPut("/cron/lock/job9", "xxx", clientv3.WithLease(leaseId))).
+		Else(clientv3.OpGet("/cron/lock/job9")) // 否则抢锁失败
+
+	// 提交事务
+	if txnResp, err = txn.Commit(); err != nil {
+		fmt.Println(err)
+		return // 没有问题
+	}
+
+	// 判断是否抢到了锁
+	if !txnResp.Succeeded {
+		fmt.Println("锁被占用:", string(txnResp.Responses[0].GetResponseRange().Kvs[0].Value))
+		return
+	}
+
+	// 2, 处理业务
+
+	fmt.Println("处理任务")
+	time.Sleep(5 * time.Second)
+
+	// 3, 释放锁(取消自动续租, 释放租约)
+	// defer 会把租约释放掉, 关联的KV就被删除了
+}
+
+func etcdWatch()  {
+	var (
+		watcher clientv3.Watcher
+		getResp *clientv3.GetResponse
+		watchStartRevision int64
+		watchRespChan <-chan clientv3.WatchResponse
+		watchResp clientv3.WatchResponse
+		event *clientv3.Event
+	)
+
+	// 模拟etcd中KV的变化
+	go func() {
+		for {
+			kv.Put(context.TODO(), "name", "xiaolin")
+
+			kv.Delete(context.TODO(), "/cron/jobs/job7")
+
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
+	// 先GET到当前的值，并监听后续变化
+	if getResp, err = kv.Get(context.TODO(), "name"); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// 现在key是存在的
+	if len(getResp.Kvs) != 0 {
+		fmt.Println("当前值:", string(getResp.Kvs[0].Value))
+	}
+
+	// 当前etcd集群事务ID, 单调递增的
+	watchStartRevision = getResp.Header.Revision + 1
+
+	// 创建一个watcher
+	watcher = clientv3.NewWatcher(client)
+
+	// 启动监听
+	fmt.Println("从该版本向后监听:", watchStartRevision)
+
+	ctx, cancelFunc := context.WithCancel(context.TODO())
+	time.AfterFunc(5 * time.Second, func() {
+		cancelFunc()
+	})
+
+	watchRespChan = watcher.Watch(ctx, "name", clientv3.WithRev(watchStartRevision))
+
+	// 处理kv变化事件
+	for watchResp = range watchRespChan {
+		for _, event = range watchResp.Events {
+			switch event.Type {
+			case mvccpb.PUT:
+				fmt.Println("修改为:", string(event.Kv.Value), "Revision:", event.Kv.CreateRevision, event.Kv.ModRevision)
+			case mvccpb.DELETE:
+				fmt.Println("删除了", "Revision:", event.Kv.ModRevision)
+			}
+		}
+	}
+
 }
 
 func etcdLease()  {
